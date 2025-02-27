fac68ab42c6ae1992ef2b95f0782a0365cb29e9d
GaZ33 <gasinho.ga@gmail.com>
2025-02-27 20:44:02 -0300

    Adicionando função loops

---
+++ semanas/semana 1/introducao/src/main/java/com/mycompany/introducao/Introducao.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.introducao;
+
+/**
+ *
+ * @author gasin
+ */
+public class Introducao {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        loops();
+    }
+    
+    public static void loops() {
+        int n = 100;
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Valor: " + i);
+        }
+        
+        
+    }
+}
