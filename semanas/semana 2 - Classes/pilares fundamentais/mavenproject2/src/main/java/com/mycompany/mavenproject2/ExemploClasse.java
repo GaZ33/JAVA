70f2758ffac6344f9c1c9c89f574d2336e2515d0
GaZ33 <gasinho.ga@gmail.com>
2025-03-16 22:59:05 -0300

    .]

--- semanas/semana 2 - Classes/pilares fundamentais/mavenproject2/src/main/java/com/mycompany/mavenproject2/ExemploClasse.java
+++ semanas/semana 2 - Classes/pilares fundamentais/mavenproject2/src/main/java/com/mycompany/mavenproject2/ExemploClasse.java
@@ -9,5 +9,37 @@ package com.mycompany.mavenproject2;
  * @author gasin
  */
 public class ExemploClasse {
+    // Criando variável privada para a clase
+    private int valor;
+    private String nome;
+    // Criando variável publica para a classe
+    public int valor2;
+    
+    
+    
+    // Criando construtor sem argumento
+    public ExemploClasse() // Inicialização da classe sem argumento
+    {
+        System.out.println("Inicialização da Classe ExemploClasse sem argumetno");
+    }
+    // Criando construtor com 1 argumetno
+    public ExemploClasse(String nome)
+    {
+        // Utilizando o this para referênciar a variável nome desse objeto
+        this.nome = nome;
+        System.out.println("Inicializando a Classe com um argumento de string: " + this.nome);
+    }
+    public ExemploClasse(int n, String nome)
+    {
+        valor2 = n;
+        valor = n;
+        this.nome = nome;
+        System.out.println("Inicializando a Classe com 2 argumentos string: " + this.nome + " e int público: " + valor2);
+    }
+    
+    
+    
+    
+    
     
 }
