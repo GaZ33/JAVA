3e5c43da91078a97cc143b4cd6c3702881c3466e
GaZ33 <gasinho.ga@gmail.com>
2025-03-30 23:45:28 -0300

    Criando o exercício da semana 3, criando a classe, atributos e métodos da classe Triangulo

---
+++ semanas/semana 3 - interfaces/exercicios/exercicio3/src/main/java/com/mycompany/exercicio3/Triangulo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exercicio3;
+
+/**
+ *
+ * @author gasin
+ */
+public class Triangulo {
+    public Double lado1;
+    public Double lado2;
+    public Double lado3;
+    
+}
